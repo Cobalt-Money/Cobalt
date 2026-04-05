b8fdf4a1941c5d69d7a16d8c08dfd44f29ae0eb5
Sriket Komali <sriketk5@gmail.com>
2026-04-04 21:53:40 -0400

    react exclision of server

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   clean: true,
   entry: "./src/index.ts",
+  external: ["react", "react-dom"],
   format: "esm",
   noExternal: [/@cobalt-web\/.*/],
   outDir: "./dist",
